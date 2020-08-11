4c8c097b52768b4e13c5cb7403d9599289d35039
Rahber Abbas Abidi <amaanabidi1234@gmail.com>
2020-08-11 19:51:05 +0530

    heroku13

--- contac/urls.py
+++ contac/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('my_admin_ncs/', admin.site.urls),
     path('',include('cont.urls'))
 ]
